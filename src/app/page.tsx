f503ee0407362a01d1909dfdb6f4d5778f618433
CupCakes05 <albinjoalex05@gmail.com>
2025-03-03 22:01:40 +0530

    import fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Products from "@/components/products";
 import Landing from "@/components/landing";
-import Pricing from "@/components/pricing";
 import { Spacer } from "@heroui/react";
 
 export default function Home() {
