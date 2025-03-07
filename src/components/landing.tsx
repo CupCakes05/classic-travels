24176e01ad9f967372394b3926544b0b28abe343
CupCakes05 <albinjoalex05@gmail.com>
2025-03-07 13:57:58 +0530

    images and drawer update

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -10,48 +10,49 @@ function Landing() {
       {/* Background Image using flex */}
       <div className="absolute top-0 left-0 w-full h-full">
         <Image
-          src={"/assets/dummy.png"}
+          src={"/assets/background.webp"}
           alt="landing page background"
           layout="fill"
           objectFit="cover"
-          className="blur-md"
+          className=""
         />
       </div>
 
       {/* Centered Text */}
-      <div className="relative z-10 min-h-screen flex justify-center items-center">
+      <div className="relative z-10 min-h-screen flex flex-col pb-5 justify-between items-center">
+        <div></div>
         <div className="text-center max-w-max">
           <h1 className="text-white text-[74px] font-bold  mb-6">
             Looking to save more on your rental Van?
           </h1>
-          <div className="bg-white p-5 text-black flex flex-col lg:flex-row items-center justify-between gap-6 rounded-xl max-h-max">
-            <h4 className="text-lg">Need to Rent a Van?</h4>
+        </div>
+        <div className="bg-white p-5 text-black flex flex-col lg:flex-row items-center justify-between gap-6 rounded-xl max-h-max">
+          <h4 className="text-lg">Need to Rent a Van?</h4>
+          <Line height="h-10" width="w-1" colour="bg-blue-500" />
+
+          <div className="flex gap-6 items-center">
+            <Input
+              label="Full Name"
+              placeholder="Enter Full Name"
+              type="text"
+              className="max-w-xs"
+            />
             <Line height="h-10" width="w-1" colour="bg-blue-500" />
 
-            <div className="flex gap-6 items-center">
-              <Input
-                label="Full Name"
-                placeholder="Enter Full Name"
-                type="text"
-                className="max-w-xs"
-              />
-              <Line height="h-10" width="w-1" colour="bg-blue-500" />
+            <Input
+              label="Mobile No"
+              placeholder="Enter Phone No."
+              type="number"
+              className="max-w-xs"
+            />
+            <Line height="h-10" width="w-1" colour="bg-blue-500" />
 
-              <Input
-                label="Mobile No"
-                placeholder="Enter Phone No."
-                type="number"
-                className="max-w-xs"
-              />
-              <Line height="h-10" width="w-1" colour="bg-blue-500" />
+            <DateRangePicker className="max-w-xs" label="Stay Duration" />
+            <Line height="h-10" width="w-1" colour="bg-blue-500" />
 
-              <DateRangePicker className="max-w-xs" label="Stay Duration" />
-              <Line height="h-10" width="w-1" colour="bg-blue-500" />
-
-              <Button color="primary" size="lg" className="text-white">
-                Book Now
-              </Button>
-            </div>
+            <Button color="primary" size="lg" className="text-white">
+              Book Now
+            </Button>
           </div>
         </div>
       </div>
